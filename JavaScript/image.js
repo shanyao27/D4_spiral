0a3bb817bb779b55417e4906b3ec88277fc448d0
andrey_olegovich <rubicknubik@gmail.com>
2025-04-23 13:08:50 +1000

    bobr

--- JavaScript/image.js
+++ JavaScript/image.js
@@ -1,6 +1,5 @@
 function createClownFace(svg) {
     let clown = svg.append("g");
-    
     clown.append("circle")
         .attr("cx", 0)
         .attr("cy", 0)
@@ -8,59 +7,49 @@ function createClownFace(svg) {
         .attr("fill", "#FFF9C4")  
         .attr("stroke", "#FF9800")  
         .attr("stroke-width", 4);
-    
     clown.append("ellipse")
         .attr("cx", -10)
         .attr("cy", -7.5)
         .attr("rx", 4)
         .attr("ry", 6)
-        .attr("fill", "lightblue");  
-    
+        .attr("fill", "lightblue");      
     clown.append("ellipse")
         .attr("cx", 10)
         .attr("cy", -7.5)
         .attr("rx", 4)
         .attr("ry", 6)
-        .attr("fill", "lightblue");
-    
+        .attr("fill", "lightblue");   
     clown.append("circle")
         .attr("cx", -10)
         .attr("cy", -7.5)
         .attr("r", 2)
-        .attr("fill", "black");
-    
+        .attr("fill", "black");   
     clown.append("circle")
         .attr("cx", 10)
         .attr("cy", -7.5)
         .attr("r", 2)
-        .attr("fill", "black");
-    
+        .attr("fill", "black");   
     clown.append("circle")
         .attr("cx", 0)
         .attr("cy", 0)
         .attr("r", 6)
-        .attr("fill", "red");
-
-    
+        .attr("fill", "red"); 
     clown.append("ellipse")
         .attr("cx", 0)
         .attr("cy", 15)
         .attr("rx",  7.5)
         .attr("ry", 4)
         .attr("fill", "red");
-    
-
     clown.append("circle")
         .attr("cx", -12.5)
         .attr("cy", 2.5)
         .attr("r", 4)
-        .attr("fill", "lightpink");
-    
+        .attr("fill", "lightpink")   
     clown.append("circle")
         .attr("cx", 12.5)
         .attr("cy", 2.5)
         .attr("r", 4)
         .attr("fill", "lightpink");
-    
+   
     return clown;
 }
--- JavaScript/path.js
+++ JavaScript/path.js
@@ -1,22 +1,20 @@
-function sprialPath() {
+function spiralPath() {
     let center = { x: 300, y: 300 };
-    let circles = 3;
-    let maxRadius = 250;
-    let points = 500;
-    let pointsArray = [];
-    
-    for (let i = 0; i <= points; i++) {
-        let t = (points - i) / points * circles * Math.PI * 2;
-        let radius = maxRadius * t / (circles * Math.PI * 2);
-        let x = center.x + radius * Math.cos(-t + Math.PI/2);
-        let y = center.y + radius * Math.sin(-t + Math.PI/2);
-        pointsArray.push({ x, y });
+    let maxRadius = 250; 
+    let points = [];
+    for (let i = 0; i <= 500; i++) {
+        let step = 1 - i / 500;
+        let angle = (3 * Math.PI / 2) + 6 * Math.PI * (1 - step);
+        let radius = maxRadius * step;
+        let x = center.x + radius * Math.cos(angle + Math.PI);
+        let y = center.y + radius * Math.sin(angle + Math.PI);
+        points.push({ x, y });
     }
-    return pointsArray;
+    return points; 
 }
 
 function drawSpiralPath(svg) {
-    let spiralPoints = sprialPath();
+    let spiralPoints = spiralPath();
     let spiralLine = d3.line()
         .x(d => d.x)
         .y(d => d.y);
